f322163d997388321b5044ceb855bedb4190961c
Neil Vallon <neilvallon@gmail.com>
2014-02-13 04:01:48 -0600

    Remove unnecessary isClient check.

--- client/monkey_rank.js
+++ client/monkey_rank.js
@@ -1,36 +1,34 @@
 Monkeys = new Meteor.Collection("monkeys");
 
-if (Meteor.isClient) {
-	Meteor.subscribe('monkeys');
-	
-	Template.monkeyForm.events({
-		"click #startMonkeys": function(event, template) {
-			var name = $("#name").val();
+Meteor.subscribe('monkeys');
+
+Template.monkeyForm.events({
+	"click #startMonkeys": function(event, template) {
+		var name = $("#name").val();
+		
+		if(name.length === 0) {
+			$('#nameGroup').addClass("has-error");
+			return;
+		} else {
+			$('#nameGroup').removeClass("has-error");
+		}
+		
+		Meteor.http.get("/hamlet.txt", function (error, result) {
+			var m = new Monkey(result.content, 50);
 			
-			if(name.length === 0) {
-				$('#nameGroup').addClass("has-error");
-				return;
-			} else {
-				$('#nameGroup').removeClass("has-error");
-			}
-			
-			Meteor.http.get("/hamlet.txt", function (error, result) {
-				var m = new Monkey(result.content, 50);
-				
-				var t = new Trainer(name, Monkeys.findOne({ name: name }), m );
-			
-				t.run();
-			});
+			var t = new Trainer(name, Monkeys.findOne({ name: name }), m );
+		
+			t.run();
+		});
+	},
+});
+
+Template.leaderboard.monkeys = function () {
+	return Monkeys.find({}, {
+		limit: 10,
+		sort: {
+			score: -1,
+			date: -1
 		},
 	});
-	
-	Template.leaderboard.monkeys = function () {
-		return Monkeys.find({}, {
-			limit: 10,
-			sort: {
-				score: -1,
-				date: -1
-			},
-		});
-	};
-}
+};
